f805ce615af0bb2d8266f46b37ac9dc6df3a42b8
mariiakhairi <mariia_khairi@inbox.ru>
2020-02-16 17:21:49 +0400

    task 2 completed before check

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,13 +3,19 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
+  public double x1;
+  public double y1;
 
-  public Point (double x, double y){
+  public Point (double x, double y, double x1, double y1){
     this.x = x;
     this.y = y;
+    this.x1 = x1;
+    this.y1 = y1;
   }
 
-
+  public double distance(){
+    return Math.sqrt((this.x1 - this.x)*(this.x1 - this.x) + (this.y1-this.y)*(this.y1-this.y));
+  }
 }
 
 
