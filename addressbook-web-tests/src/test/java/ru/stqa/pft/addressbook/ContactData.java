02c3287c1b04c5b049d893523140a8be79d0317d
mariiakhairi <mariia_khairi@inbox.ru>
2020-02-21 21:15:36 +0400

    Add create contact test

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,67 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String surname;
+  private final String position;
+  private final String company;
+  private final String address;
+  private final String phone;
+  private final String email;
+  private final String day;
+  private final String month;
+  private final String year;
+
+  public ContactData(String name, String surname, String position, String company, String address, String phone, String email, String day, String month, String year) {
+    this.name = name;
+    this.surname = surname;
+    this.position = position;
+    this.company = company;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+    this.day = day;
+    this.month = month;
+    this.year = year;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public String getPosition() {
+    return position;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getDay() {
+    return day;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public String getYear() {
+    return year;
+  }
+}
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/CreateContactTests.java
@@ -0,0 +1,114 @@
+package ru.stqa.pft.addressbook;
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class CreateContactTests {
+  private WebDriver wd;
+
+  @BeforeMethod(alwaysRun = true)
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver();
+    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String userName, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(userName);
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+
+  @Test
+  public void testCreateContact() throws Exception {
+    gotoCreateContactPage();
+    fillContactForm(new ContactData("Mariia", "Khairi", "QA", "Oracle", "IMPZ,LagoVista A", "0559470842", "mariia_khairi@inbox.com", "9", "May", "1989"));
+    submitContactForm();
+    gotoHomePage();
+    logout();
+  }
+
+  private void logout() {
+    wd.findElement(By.linkText("Logout")).click();
+  }
+
+  private void gotoHomePage() {
+    wd.findElement(By.linkText("home page")).click();
+  }
+
+  private void submitContactForm() {
+    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+  }
+
+  private void fillContactForm(ContactData contactData) {
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contactData.getName());
+    wd.findElement(By.name("middlename")).click();
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contactData.getSurname());
+    wd.findElement(By.name("title")).click();
+    wd.findElement(By.name("title")).clear();
+    wd.findElement(By.name("title")).sendKeys(contactData.getPosition());
+    wd.findElement(By.name("company")).click();
+    wd.findElement(By.name("company")).clear();
+    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+    wd.findElement(By.name("address")).click();
+    wd.findElement(By.name("address")).clear();
+    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
+    wd.findElement(By.name("home")).click();
+    wd.findElement(By.name("home")).clear();
+    wd.findElement(By.name("home")).sendKeys(contactData.getPhone());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    wd.findElement(By.name("bday")).click();
+    new Select(wd.findElement(By.name("bday"))).selectByVisibleText(contactData.getDay());
+    wd.findElement(By.name("bday")).click();
+    wd.findElement(By.name("bmonth")).click();
+    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText(contactData.getMonth());
+    wd.findElement(By.name("bmonth")).click();
+    wd.findElement(By.name("byear")).click();
+    wd.findElement(By.name("byear")).clear();
+    wd.findElement(By.name("byear")).sendKeys(contactData.getYear());
+  }
+
+  private void gotoCreateContactPage() {
+    wd.findElement(By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void tearDown() throws Exception {
+    wd.quit();
+
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      wd.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
